3f84abe0a4020ebbdbe949f4c9c0b8af46dc348b
Batorshin Artem <NorvellArt@gmail.com>
2026-07-22 23:01:47 +0300

    daytime client

---
+++ lib/unp.h
@@ -0,0 +1,21 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define MAXLINE 4096 /* max text line length */
+
+/* Define bzero() as a macro if it's not in standard C library. */
+#ifndef HAVE_BZERO
+#define bzero(ptr, n) memset(ptr, 0, n)
+#endif
+
+/* Following shortens all the type casts of pointer arguments */
+#define SA struct sockaddr
+
+void err_quit(const char *, ...);
+void err_sys(const char *, ...);
