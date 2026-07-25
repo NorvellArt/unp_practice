71bb3a4bfc75c00eefd91fee48828f6a9af89c29
Batorshin Artem <NorvellArt@gmail.com>
2026-07-25 18:59:02 +0300

    add byteorder util

--- lib/unp.h
+++ lib/unp.h
@@ -19,6 +19,28 @@
 /* Following shortens all the type casts of pointer arguments */
 #define SA struct sockaddr
 
+#if defined(__x86_64__) || defined(_M_X64)
+  #define HOST_CPU "x86_64"
+#elif defined(__i386__) || defined(_M_IX86)
+  #define HOST_CPU "i686"
+#elif defined(__aarch64__)
+  #define HOST_CPU "aarch64"
+#else
+  #define HOST_CPU "unknown"
+#endif
+
+#if defined(__linux__)
+  #define HOST_OS "linux-gnu"
+#elif defined(__APPLE__)
+  #define HOST_OS "darwin"
+#elif defined(_WIN32)
+  #define HOST_OS "windows"
+#else
+  #define HOST_OS "unknown"
+#endif
+
+#define CPU_VENDOR_OS HOST_CPU "-pc-" HOST_OS
+
 int Socket(int, int, int);
 void Bind(int, const SA *, socklen_t);
 void Listen(int, int);
